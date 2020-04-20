ce4aad53847234a1d5115db61c7ab588d1cb7256
Aaron Hooper <rnjckhpr@gmail.com>
2020-04-20 23:41:28 +0100

    Add curses

--- synonym.py
+++ synonym.py
@@ -1,10 +1,14 @@
-from urllib.parse import urljoin, urlparse, urlunparse
+import curses
 from os.path import join
+import sys
+import time
+from urllib.parse import urljoin, urlparse, urlunparse
 
 import requests
 
 
 RES_FORMAT = "json"
+ENABLE_API = False
 API_KEY_FILE = "api_key"
 
 DUMMY_RESULTS = {'liquid': ['swimming', 'limpid', 'melted', 'liquified', 'fluent', 'fluid', 'smooth', 'liquidness', 'liquidity', 'liquid state', 'consonant', 'fluid', 'state', 'state of matter'], 'great': ['outstanding', 'bang-up', 'bully', 'corking', 'cracking', 'dandy', 'groovy', 'keen', 'neat', 'nifty', 'peachy', 'slap-up', 'swell', 'smashing', 'capital', 'majuscule', 'enceinte', 'expectant', 'gravid', 'avid', 'big', 'eager', 'heavy', 'large', 'not bad', 'with child', 'zealous', 'achiever', 'succeeder', 'success', 'winner'], 'forest': ['wood', 'woods', 'woodland', 'timberland', 'timber', 'biome', 'botany', 'dry land', 'earth', 'flora', 'ground', 'land', 'solid ground', 'terra firma', 'vegetation', 'afforest', 'plant', 'set']}
@@ -42,13 +46,67 @@ def _get_synonyms(*words, **options):
 
 
 def get_synonyms(*words):
-    return _get_synonyms(*words, should_hit_api=True)
+    return _get_synonyms(*words, should_hit_api=ENABLE_API)
 
 
-def main():
+def main(screen):
     words = ["liquid", "great", "forest"]
-    print(get_synonyms(*words))
 
+    # Get terminal size
+    max_y, max_x = screen.getmaxyx()
     
+    # Set up line windows
+    top_line = curses.newwin(1, max_x, 0, 0)
+    middle_line = curses.newwin(1, max_x, round(max_y / 2), 0)
+    bottom_line = curses.newwin(1, max_x, max_y - 1, 0)
+
+    # Hide cursor
+    curses.curs_set(0)
+
+    bottom_line.addstr(0, 0, "Fetching results ...")
+    bottom_line.refresh()
+    synonyms = get_synonyms(*words)
+
+    # Simulate latency
+    time.sleep(2)
+
+    # Show cursor
+    curses.curs_set(1)
+
+    screen.erase()
+    shortlist = {}
+
+    for word in synonyms:
+        top_line.erase()
+        top_line.addstr(0, 0, "Synonym for")
+        top_line.addstr(0, 12, f"{word}", curses.A_BOLD)
+        top_line.refresh()
+ 
+        for synonym in synonyms[word]:
+            middle_line.erase()
+            middle_line.addstr(0, 2, f"==> ")
+            middle_line.addstr(0, 7, synonym, curses.A_BOLD)
+            middle_line.refresh()
+            bottom_line.addstr(0, 0, "Shortlist word? (y/n/d/q/?) ")
+
+            while True:
+                c = bottom_line.getch()
+
+                if c == ord('y'):
+                    shortlist[word] = synonym
+                    break
+                elif c == ord('n'):
+                    break
+                elif c == ord('q'):
+                    sys.exit(0)
+
+    screen.clear()
+    screen.addstr(0, 0, str(shortlist))
+    screen.refresh()
+
+    while True:
+        continue
+
+
 if __name__ == "__main__":
-    main()
+    curses.wrapper(main)
